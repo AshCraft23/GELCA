9fbd8a764672498204a765fd03e2b24e60558ae0
AshCraft23 <90273368+AshCraft23@users.noreply.github.com>
2025-11-30 01:24:33 -0500

    Delete backend directory

--- backend/src/routes/health.routes.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from "express";
-
-const router = Router();
-
-router.get("/", (req, res) => {
-  res.json({ ok: true, message: "Backend funcionando" });
-});
-
-export default router;
--- backend/src/routes/reportes.routes.ts
+++
@@ -1,8 +0,0 @@
-import { Router } from "express";
-import { reporteCosechas } from "../controllers/reportes.controller";
-
-const router = Router();
-
-router.get("/cosechas", reporteCosechas);
-
-export default router;
